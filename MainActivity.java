cfe4713e9d2809ce95e9af16f236d0d6b8e59301
Дамир <damir180501@mail.ru>
2017-10-25 16:37:40 +0300

    Я подключился к GitHub

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package student8.example.com.clock_tok;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class MainActivity extends Activity {
+
+    Button btn;
+    TextView tv, tv2;
+    Date date = new Date();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.textView);
+        tv = (TextView) findViewById(R.id.textView2);
+
+        //tv2.setText(date.getTime() + " ");
+        date.setTime(10);
+        tv.setText(date.toString());
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ SecondActivity.java
@@ -0,0 +1,8 @@
+package student8.example.com.clock_tok;
+
+/**
+ * Created by student2 on 23.10.17.
+ */
+
+public class SecondActivity {
+}
